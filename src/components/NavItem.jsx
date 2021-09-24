d420ffefa1351b37b264356f96aeeb1ce088a712
Charlie Gerard <annecharlotte.gerard@gmail.com>
2021-09-24 14:31:16 -0700

    fix loading images

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,13 +1,26 @@
 import { Link } from "solid-app-router";
 import styles from "./NavItem.module.css";
+import menuIcon from "../assets/menu-icon.svg";
+import aboutIcon from "../assets/about-icon.svg";
+import galleryIcon from "../assets/gallery-icon.svg";
 
 export default function NavItem({ text }) {
+  const getIcon = (text) => {
+    switch (text.toLowerCase()) {
+      case "gallery":
+        return galleryIcon;
+      case "menu":
+        return menuIcon;
+      case "about":
+        return aboutIcon;
+    }
+  };
   return (
     <>
       <li className={styles.navItem}>
         <Link href={`/${text.toLowerCase()}`}>
           <a className={styles.navItemLink}>
-            <img src={`/${text.toLowerCase()}-icon.svg`} alt={text} />
+            <img src={getIcon(text)} alt={text} />
             <span>{text}</span>
           </a>
         </Link>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import Nav from "./Nav.jsx";
+import navLogo from "../assets/logo.svg";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar">
-      <img src="./assets/logo.svg" alt="" />
+    <aside className={styles.sidebar}>
+      <img src={navLogo} alt="" />
       <Nav />
-      <footer className="main-footer">
+      <footer className={styles.mainFooter}>
         <p className="built-with-text">Built with Next.js and Netlify</p>
       </footer>
     </aside>
